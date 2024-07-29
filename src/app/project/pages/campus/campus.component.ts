b3ed0b8091e9dc953d5921115f074b9e667eb0b5
Gonzalo Madariaga <gonzalo.madariaga@uv.cl>
2024-07-29 12:48:46 -0400

    creacion modulo y componente campus

---
+++ src/app/project/pages/campus/campus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campus',
+  templateUrl: './campus.component.html',
+  styles: [
+  ]
+})
+export class CampusComponent {
+
+}
